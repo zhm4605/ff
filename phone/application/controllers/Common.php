00c3b0812d7650a85a705ef8f01f15b828f220f0
zhm <460569137@qq.com>
2017-02-15 17:01:11 +0800

    20170215

--- phone/application/controllers/Common.php
+++ phone/application/controllers/Common.php
@@ -9,6 +9,7 @@ class Common extends MY_Controller {
 	}
 
   public function index(){
+  	get('http://api.k780.com:88/?app=finance.rate&scur=USD&tcur=CNY&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4')
   }
 
  
